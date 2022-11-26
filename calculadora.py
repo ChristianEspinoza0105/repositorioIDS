d71d2b71f5a340dc9322644baad6328e40dd915d
Christian Espinoza <cristian_cris85@outlook.com>
2022-11-26 03:22:30 -0600

    Cuarto commit

--- calculadora.py
+++ calculadora.py
@@ -16,9 +16,10 @@ while True:
 if opcion == 1:
     print(" ")
     print("RESULTADO: La suma de",n1,"+",n2,"es igual a",n1+n2)
+elif opcion == 2:
+    print(" ")
+    print("RESULTADO: La resta de",n1,"-",n2,"es igual a",n1-n2)
 elif opcion == 3:
     print(" ")
     print("RESULTADO: El producto de",n1,"*",n2,"es igual a",n1*n2)
-
-
     print("Opción incorrecta")
