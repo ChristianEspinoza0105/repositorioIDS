85fdc67e87bc1d4889f7f825b4cd59f4464ed362
Arleon18 <edsonarriaganv@gamil.com>
2022-11-29 12:29:30 -0600

    Calculadora completa

--- calculadora.py
+++ calculadora.py
@@ -9,21 +9,33 @@ while True:
     1) Sumar los dos números
     2) Restar los dos números
     3) Multiplicar los dos números
-    4) Cambiar los números elegidos
-    5) Apagar calculadora
+    4) Dividir
+    5) Creditos 
+    6) Apagar calculadora
     """)
     opcion = int(input("Elige una opción: ") )     
-if opcion == 1:
-    print(" ")
-    print("RESULTADO: La suma de",n1,"+",n2,"es igual a",n1+n2)
-elif opcion == 2:
-    print(" ")
-    print("RESULTADO: La resta de",n1,"-",n2,"es igual a",n1-n2)
-elif opcion == 3:
-    print(" ")
-    print("RESULTADO: El producto de",n1,"*",n2,"es igual a",n1*n2)
-elif opcion == 4:
-        n1 = float(input("Introduce tu primer número: ") )
-        n2 = float(input("Introduce tu segundo número: ") )
-elif opcion == 5:
-    print("Opción incorrecta")
+
+    if opcion == 1:
+        print(" ")
+        print("RESULTADO: La suma de",n1,"+",n2,"es igual a",n1+n2)
+    elif opcion == 2:
+        print(" ")
+        print("RESULTADO: La resta de",n1,"-",n2,"es igual a",n1-n2)
+    elif opcion == 3:
+        print(" ")
+        print("RESULTADO: El producto de",n1,"*",n2,"es igual a",n1*n2)
+    elif opcion == 4:
+        print(" ")
+        print("RESULTADO: El producto de",n1,"/",n2,"es igual a",n1/n2)
+    elif opcion == 5:
+        print("""
+        Equipo:
+        EDSON DANIEL ARRIAGA LOPEZ.
+        ADRIAN DIAZ PEREYRA.
+        EROZ ERNESTO VILLALPANDO MUÑOZ.
+        GUSTAVO CHRISTIAN ESPIOZA CUEVAS.
+        CARLOS ISAAC RAMIREZ MARTINEZ.""")
+    elif opcion == 6:
+        break
+    else:
+        print("Opción incorrecta")
